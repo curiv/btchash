d4c4f8b993b2a5900c37eacb0dacbafd3bbf53b7
Curiv <>
2020-05-07 22:33:02 +0500

    Added reade

--- hashminer.py
+++ hashminer.py
@@ -1,21 +1,22 @@
 import os, hashlib
-
 def genblock(message, complexity):
     print(f"Initial message: {message}")
     encoded_message = message.encode()
 
     # Initialize static padding and hash_block 
-    N = complexity
+    N       = complexity
     padding = hash_block =  b'0'*(N+1)
 
     # Keep trying to find a correct padding to accord needed complexity
     # The first N'th elements must be zeroes but N+1 is not
     while (hash_block[:N] != '0'*N or hash_block[N] == '0'):
         print(f"hash of a message: {hash_block}")
+
+        # Get the sha hash
         hash_block = hashlib.sha256(encoded_message + padding).hexdigest()
 
         # Randomize padding
-        padding  = os.urandom(16)
+        padding    = os.urandom(16)
 
     print(f"RESULT: {hash_block}")
 
@@ -24,6 +25,6 @@ if  __name__ == "__main__":
     message = "Alice sent 10 btc to Bob"
 
     # Complexity is a number of zeroes at the beginning 
-    complexity = 5
+    complexity = 6
 
     genblock(message, complexity)
